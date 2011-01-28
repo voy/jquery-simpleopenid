4129ec291461a5e4d51009aa21231ff4b4663665
Vojtech Jasny <voy@voy.cz>
2011-01-28 16:19:59 +0100

    Added configurable providers and login via google apps.

--- jquery.simpleopenid.js
+++ jquery.simpleopenid.js
@@ -1,81 +1,95 @@
 (function($)
 {
-	// This script was written by Steve Fenton
+	// Bases on a script written by Steve Fenton
 	// http://www.stevefenton.co.uk/Content/Jquery-Simple-Openid/
+	//
+	// Modified by Vojtech Jasny <voy@voy.cz>
+	//
 	// Feel free to use this jQuery Plugin
-	// Version: 1.0.0
-	
+	// Version: 1.1.0
+
 	$.fn.simpleopenid = function (settings) {
-	
+
 		var config = {
 			classmodifier: "oid",
-			baseaddress: ""
+			baseaddress: "",
+			providers: null
 		};
-		
+
 		if (settings) {
 			$.extend(config, settings);
 		}
-		
-		var provider = new Array();
+
 		// ["Name", "Image", "Uri", "Requires User Name"]
-		provider[provider.length] = ["Google", "openid/google.png", "https://www.google.com/accounts/o8/id", false];
-		provider[provider.length] = ["Yahoo", "openid/yahoo.png", "http://yahoo.com/", false];
-		provider[provider.length] = ["AOL", "openid/aol.png", "http://openid.aol.com/{0}", true];
-		provider[provider.length] = ["My Open Id", "openid/myopenid.png", "http://{0}.myopenid.com/", true];
-		provider[provider.length] = ["Flickr", "openid/flickr.png", "http://flickr.com/{0}/", true];
-		provider[provider.length] = ["Myspace", "openid/myspace.png", "http://www.myspace.com/{0}", true];
-		provider[provider.length] = ["Technorati", "openid/technorati.png", "http://technorati.com/people/technorati/{0}/", true];
-		provider[provider.length] = ["Wordpress", "openid/wordpress.png", "http://{0}.wordpress.com", true];
-		provider[provider.length] = ["Blogspot", "openid/blogspot.png", "http://{0}.blogspot.com/", true];
-		provider[provider.length] = ["Live Journal", "openid/livejournal.png", "http://{0}.livejournal.com", true];
-		provider[provider.length] = ["ClaimID", "openid/claimid.png", "http://claimid.com/{0}", true];
-		provider[provider.length] = ["Vidoop", "openid/vidoop.png", "http://{0}.myvidoop.com/", true];
-		provider[provider.length] = ["Verisign", "openid/verisign.png", "http://{0}.pip.verisignlabs.com/", true];
+		var providers = {
+			"google":       ["Google", "openid/google.png", "https://www.google.com/accounts/o8/id", false],
+			"google_apps":  ["Google Apps", "openid/google.png", "https://www.google.com/a/{0}/o8/ud?be=o8", true],
+			"yahoo":        ["Yahoo", "openid/yahoo.png", "http://yahoo.com/", false],
+			"aol":          ["AOL", "openid/aol.png", "http://openid.aol.com/{0}", true],
+			"myopenid":   ["My Open Id", "openid/myopenid.png", "http://{0}.myopenid.com/", true],
+			"flickr":       ["Flickr", "openid/flickr.png", "http://flickr.com/{0}/", true],
+			"myspace":      ["Myspace", "openid/myspace.png", "http://www.myspace.com/{0}", true],
+			"technorati":   ["Technorati", "openid/technorati.png", "http://technorati.com/people/technorati/{0}/", true],
+			"wordpress":    ["Wordpress", "openid/wordpress.png", "http://{0}.wordpress.com", true],
+			"blogspot":     ["Blogspot", "openid/blogspot.png", "http://{0}.blogspot.com/", true],
+			"live_journal": ["Live Journal", "openid/livejournal.png", "http://{0}.livejournal.com", true],
+			"claimid":      ["ClaimID", "openid/claimid.png", "http://claimid.com/{0}", true],
+			"vidoop":       ["Vidoop", "openid/vidoop.png", "http://{0}.myvidoop.com/", true],
+			"verisign":     ["Verisign", "openid/verisign.png", "http://{0}.pip.verisignlabs.com/", true],
+		}
+
+		var userProviders = {};
+		if (config.providers) {
+			$.each(config.providers, function(i, value) {
+				userProviders[value] = providers[value];
+			});
+		} else {
+			userProviders = providers;
+		}
 
 		return this.each(function () {
-			
-			var $Element = $(this);
-			
-			var providerHtml = "<ul>";
-			
-			for (var i = 0; i < provider.length; i++) {
-				providerHtml += "<li>";
-				
-				if (provider[i][1].length > 0) {
-					providerHtml += '<img src="' + config.baseaddress + provider[i][1] + '" alt="' + provider[i][0] + '" title="' + provider[i][0] + '" rel="' + i + '" class="' + config.classmodifier + '" style="cursor: pointer;">';
-				}
-				
-				providerHtml += "</li>";
-			}
-			
-			providerHtml += "</ul>";
-			
-			$Element.prepend(providerHtml);
-			
-			$Element.submit( function () {
+			var $Element = $(this),
+			    providerList = $("<ul>");
+
+			$.each(userProviders, function(i, provider) {
+				providerList.append($("<li>", {
+					html: $("<img>", {
+						src: config.baseaddress,
+						alt: provider[0],
+						title: provider[0],
+						'class': config.classmodifier,
+						css: { cursor: 'pointer' },
+						data: { provider: provider }
+					})
+				}));
+			});
+
+			$Element.prepend(providerList);
+
+			$Element.submit(function () {
 				var uri = $(this).find("#openid_identifier").val();
 				if (uri.indexOf("{0}") > -1) {
 					$(this).find("#openid_identifier").val(uri.replace("{0}", $(this).find("#openid_username").val()));
 				}
 			});
-			
+
 			// Handle the selection click
-			$Element.find("img." + config.classmodifier).click( function () {
-			
-				var i = $(this).attr("rel");
-				var title = provider[i][0];
-				var image = provider[i][1];
-				var uri = provider[i][2];
-				var hasUser = provider[i][3];
-				
+			$Element.find("img." + config.classmodifier).click(function () {
+
+				var provider = $(this).data("provider"),
+				    title = provider[0],
+				    image = provider[1],
+				    uri = provider[2],
+				    hasUser = provider[3];
+
 				$("img.selected").removeClass("selected");
 				$(this).addClass("selected");
-				
+
 				if (hasUser) {
 					// User name required
 					$Element.find("fieldset").eq(0).show();
 					$Element.find("fieldset").eq(1).hide();
-					
+
 					$("span." + config.classmodifier).remove();
 
 					var $UserName = $Element.find("#openid_username");
@@ -88,7 +102,7 @@
 					} else {
 						$UserName.before('<span class="' + config.classmodifier + '">' + uriParts[0] + '</span>');
 					}
-					
+
 					$Element.find("#openid_identifier").val(uri);
 				} else {
 					// No user name required
@@ -97,9 +111,9 @@
 					$Element.find("#openid_identifier").val(uri);
 					$Element[0].submit();
 				}
-				
+
 			});
-			
+
 		});
 	};
 })(jQuery);
